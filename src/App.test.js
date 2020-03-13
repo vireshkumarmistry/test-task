d34ff615f2aae2a866557f0fe32fa14f8208d166
Viresh <vireshkumarmistry@gmail.com>
2020-03-13 17:45:12 +0530

    Add Tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render} from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders header with text: "Test Task"', () => {
+  const {getByText} = render(<App/>);
+  const headerElement = getByText(/Test Task/i);
+  expect(headerElement).toBeInTheDocument();
 });
